654c1ad4e5377d22d84b3c7b75f1282d3e8a3fb0
Ben Cai <bencai@Bens-MacBook-Air.local>
2021-07-24 12:26:41 +0800

    Add: homepage basic add

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
 import "./App.css";
 
+import HomePage from "./component/homepage/homepage.component";
+
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <HomePage />
+    </div>
+  );
 }
 
 export default App;
